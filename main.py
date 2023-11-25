e3aed9c2742c533810d80a26448c1151a5285ed3
Speedy <148458995+speedyownjoo@users.noreply.github.com>
2023-11-25 13:17:36 -0600

    clean up imports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from logging import Filter, getLogger
+from logging import Filter, getLogger, Logger
 from logging.config import dictConfig
 from pathlib import Path
 from yaml import safe_load
@@ -6,23 +6,23 @@ from yaml import safe_load
 
 def configure_logging() -> None:
     with open('logging.yaml', 'r') as yaml:
-        log_cfg = safe_load(yaml.read())
+        log_cfg: dict = safe_load(yaml.read())
 
     for key, value in log_cfg.get('handlers').items():
-        file_path = value.get('filename')
+        file_path: str = value.get('filename')
         if not file_path:
             continue
-        log_file = Path(file_path)
+        log_file: Path = Path(file_path)
         log_file.parent.mkdir(exist_ok=True, parents=True)
     dictConfig(log_cfg)
-    logger = getLogger(__name__)
+    logger: Logger = getLogger(__name__)
     logger.info(f'logging initialized')
 
 
 def main():
     configure_logging()
     log_name: str = 'vehicle'
-    logger = getLogger(log_name)
+    logger: Logger = getLogger(log_name)
     veh_filter: Filter = Filter(log_name)
     logger.addFilter(veh_filter)
     logger.info(f'should appear in vehicle.log only')
